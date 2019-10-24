7b6e51e0efbb7630e169bb5945417aaeeca9d2c4
MuhammadTahaAzam1997 <muhammadtahaazam@gmail.com>
2019-10-24 11:29:00 -0700

    Creating the main class for Jaava Codes

---
+++ JAVAPractice.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapractice;
+
+/**
+ *
+ * @author Aksystems
+ */
+public class JAVAPractice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   
+        System.out.println("This is first Program for the practice");
+        printingstatement obj=new printingstatement();
+        obj.printingstatement();
+        UserinputScanner obj1= new UserinputScanner();
+        obj1.inputString();
+        obj1.inputChar();
+        obj1.printallvariable();
+    }
+    
+}
