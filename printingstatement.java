2959558126cdb2d79221d11fe0586fe573bbd596
MuhammadTahaAzam1997 <muhammadtahaazam@gmail.com>
2019-10-24 11:32:25 -0700

    Printing The Statement by S.O.U.T

---
+++ printingstatement.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapractice;
+
+/**
+ *
+ * @author Aksystems
+ */
+public class printingstatement {
+
+public void printingstatement(){
+    
+    System.out.println("This statement is coming from the the printing statement class");
+}    
+}
