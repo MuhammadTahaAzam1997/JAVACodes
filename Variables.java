e96f3db036eda772d801ca833d4ae269a4abc08a
MuhammadTahaAzam1997 <muhammadtahaazam@gmail.com>
2019-10-24 11:56:02 -0700

    Working on Variables in JAVA

---
+++ Variables.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javapractice;
+
+/**
+ *
+ * @author Aksystems
+ */
+public class Variables {
+
+public Variables(){
+    int age=22;
+    double Gpa=3.2;
+    String uni="SSUET";
+    short year=2019;
+    char sec='A';
+    
+    System.out.println("Name: Muhammad Taha Azam/nAge:"+age+"\nGpa:"+Gpa+"\nUniversity name:"+uni+"\nyear:"+year+"\nsec:"+sec);
+}    
+}
